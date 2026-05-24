cab4844eb80301229c86e8d48d225f019d9f6671
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-05-24 22:19:33 +0600

    added problem 5 solution

---
+++ src/Problem-Solving-5.ts
@@ -0,0 +1,25 @@
+// Remove duplicate values from an array and return a new array.
+
+// option :1
+//    const numberArr:number[]=[1, 2, 2, 3, 4, 4]
+// const newArry=[...new Set(numberArr)]
+// console.log(newArry)
+
+
+// option:2
+const numberArr: number[] = [1, 2, 2, 3, 4, 4];
+
+// const newArray: number[] = [];
+
+// for (let i = 0; i < numberArr.length; i++) {
+//   if (!newArray.includes(numberArr[i])) {
+//     newArray.push(numberArr[i]);
+//   }
+// }
+
+// console.log(newArray);
+
+
+
+
+
