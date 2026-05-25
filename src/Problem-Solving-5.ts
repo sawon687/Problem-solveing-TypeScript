5a89edca64d571898598050b21a1fa7ca9afdd4f
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-05-25 21:45:12 +0600

    added problem 6 and soution

--- src/Problem-Solving-5.ts
+++ src/Problem-Solving-5.ts
@@ -38,3 +38,6 @@ console.log(newArry)
 
 
 
+
+
+
---
+++ src/Problem-Solving-6.ts
@@ -0,0 +1,36 @@
+// 6. Two Sum
+// Given an array of integers and a target number, return the indices of two numbers whose sum equals the target.
+
+// Constraints:
+
+// Each input has exactly one solution.
+// You cannot use the same element twice.
+// Example:
+
+// Input:
+// nums = [2, 7, 11, 15]
+// target = 9
+
+// Output:
+// [0, 1]
+
+
+const twoSum=(array:number[],target:number)=>{
+   
+    for(let i=0;i<array.length; i++)
+    {
+       for(let j=i+1 ; j<array.length ; j++)
+       {
+            if(array[i]+array[j]===target)
+            {
+               
+              return  [i,j]
+            }
+       }
+    }
+
+
+
+}
+
+console.log(twoSum( [2, 7, 11, 15],9))
